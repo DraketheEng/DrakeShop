2ba005c8e4ae911f1b0b489d37b8e823f2438471
Can Ağırbaş <agirbas06@outlook.com>
2024-07-17 13:25:28 +0300

    new features

--- DrakeShop/IdentityServer/DrakeShop.IdentityServer/obj/Debug/net6.0/DrakeShop.IdentityServer.AssemblyInfo.cs
+++ DrakeShop/IdentityServer/DrakeShop.IdentityServer/obj/Debug/net6.0/DrakeShop.IdentityServer.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DrakeShop.IdentityServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44764841d5c8aee32b3fdc495f7eb7d82e952f45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b32113d16102bbf701b39657064a6e2577a5a73e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DrakeShop.IdentityServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DrakeShop.IdentityServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
